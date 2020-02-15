573fa7cef39aeb699b1533dd8387667a35906df2
Glen Sargent <glensarg@hotmail.co.uk>
2020-02-15 17:43:37 +0000

    implement base ecs and create base components

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
-use rltk::{Rltk, GameState, Console};
+use rltk::{Rltk, GameState, Console, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+#[macro_use]
+extern crate specs_derive;
 
-// Create game state struct
-struct State {}
+// experimental roguelike game using ecs...
+
+// Create game state struct with the ecs world
+struct State {
+    ecs: World
+}
 // Create the main tick method
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
+        ctx.print(1, 1, "Test");
     }
+}
+// Create ecs component for unit positions
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+// Create ecs component for renderable components
+#[derive(Component)]
+struct Renderable {
+    glyph: u8,
+    fg: RGB,
+    bg: RGB,
 }
 
 fn main() {
@@ -19,7 +40,13 @@ fn main() {
         .build();
 
     // initialize game state
-    let gs = State{};
+    let mut gs = State{
+        ecs: World::new()
+    };
+
+    // tell ecs about the components we have created
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
 
     // run the core loop
     rltk::main_loop(context, gs);
